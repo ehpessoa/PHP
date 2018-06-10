e561fc0c90464b27880027190311d46caf4852b3
Everaldo Pessoa <everaldo.pessoa@gmail.com>
2018-06-10 14:05:54 -0300

    Update config.php

--- Twitter/config.php
+++ Twitter/config.php
@@ -1,5 +1,5 @@
 <?php
-define('CONSUMER_KEY', 'XYkc0AGkf6qUJYpspLo1fRGIv');
-define('CONSUMER_SECRET', 'iAaTBgqO5lw66OlesGs04rqdR6zvCTZ2IBP35c38oKzoPf2JPV');
+define('CONSUMER_KEY', 'XYAGkf6qpspLo1fRGIv');
+define('CONSUMER_SECRET', 'iAaTBgqO5lw66OGs0R6zvCTZ35c38oKzoPf2JPV');
 define('OAUTH_CALLBACK', 'http://apps.ehpessoa.com/twitter/process.php');
 ?>
